0d0a66bcdb35ef7c176285e458531830ce8c052f
Millad Ayoubi <Milladayoubi@gmail.com>
2023-06-14 23:13:51 -0400

    Card Details

--- client/src/app/components/BobaDetails.js
+++ client/src/app/components/BobaDetails.js
@@ -3,13 +3,14 @@ import Image from 'next/image';
 
 import SizeSelection from './SizeSelection'
 import SugerSelection from './SugerSelection';
+import Topping from './Topping';
 
 const BobaDetails = ({ boba }) => {
     console.log(boba)
 
-    const [size, setSize] = useState('small')
+    const [size, setSize] = useState('medium')
 
-    const [suger, setSuger] = useState('100% Suger')
+    const [suger, setSuger] = useState('100% ')
 
     const [toppings, setToppings] = useState([])
 
@@ -42,10 +43,47 @@ const BobaDetails = ({ boba }) => {
 
     return (
     
-         <div> 
-            <div>
-                <Image width={450} height={450} src={boba.image}/>
+         <div className='flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8'> 
+            <div className='lg:flex-1 flex justify-center items-center'>
+                <div className='max-w-[300px] lg:max-w-none mt-6 lg:mt-0'>
+                <Image width={450} height={450} src={boba.image} alt='' priorty={1} className='mx-auto relative'/>
             </div>
+            </div>
+
+
+        <div className='bg-pink-100 flex flex-col flex-1'>
+            <div className='flex-1 p-2 text-center lg:text-left'>
+                <div className='flex-1 bg-white overflow-y-scroll h-[46vh] scrollbar-thin scrollbar-thumb-grey-500 scrollbar-track-white pr-2 '>
+                    <div className='font-semibold'>
+                        <h2 className='capitalize text-3xl mb-1'>{boba.name}</h2>
+                        <div className='bg-yellow-200 mb-6 text-lg font-medium'>
+                            <span>{ size === 'small' ? '350 ml' : size === 'medium' ? '500 ml' :
+                            size === 'large' ? '700 ml' : null }</span>
+                            <span>, {suger} Suger</span>
+                        </div>
+                    </div>
+
+                    <SizeSelection boba={boba} size={boba} setSize={setSize}/> 
+                    <SugerSelection/>
+                    <div>Choose Toppings</div>
+                    <div>
+                        {boba.toppings?.map((topping, index) => {
+                            return <Topping key={index}/>
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-full flex items-center px-2 lg:items-end'>
+                <button className='btn btn-lg gradient w-full flex justify-center gap-x-2'>
+                <div>Add To Cart For</div>
+                <div>$ {price} </div> 
+                    
+               
+                
+                </button>
+            </div>
+        </div>
         </div>
     );
 };
--- client/src/app/components/SizeSelection.js
+++ client/src/app/components/SizeSelection.js
@@ -1,5 +1,23 @@
-const SizeSelection = () => {
-  return <div>SizeSelection</div>;
+"use client";
+import Image from 'next/image'
+
+const SizeSelection = ( {boba, size, setSize}) => {
+
+  return (
+  
+  <div className='bg-indigo-400'>
+    
+     <div>
+      <label className='bg-red-200'>
+        <Image  className={`${size === 'small' ? 'border-2 border-orange p-[2px] rounded-full' : 'border-transparent filter saturate-[.1]' }`} src={boba.image} width={60} height={60} alt=''/>
+        <input type="radio" name='size' value='small' checked={size === 'small'} onChange={(e) => setSize(e.target.value)} className='apperance-none' />
+      </label>
+     </div>
+     
+     
+  </div>
+  
+  );
 };
 
 export default SizeSelection;
