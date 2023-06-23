968508998fcbf59d0384cd6845f5064d95f52086
Millad Ayoubi <Milladayoubi@gmail.com>
2023-06-23 17:40:21 -0400

    Responsivness Update

--- client/src/app/components/Banner.js
+++ client/src/app/components/Banner.js
@@ -7,11 +7,11 @@ import {
 
 const Banner = () => {
     return (
-    <section className=" bg-pattern bg-no-repeat bg-cover lg:min-h-[768px] pt-16 lg:pt-16 relative bottom-16">
-      <div className="container mx-auto min-h-[788px]  flex items-center justfy-center relative top-20 lg:top-0 lg:bottom-12">
+    <section className=" bg-pattern bg-no-repeat bg-cover lg:min-h-[768px]  pt-16 lg:pt-16 relative bottom-16">
+      <div className="container mx-auto lg:min-h-[788px] min-h-[620px]  flex items-center justfy-center relative top-20 lg:top-0 lg:bottom-12">
         <MouseParallaxContainer globalFactorX={0.1} globalFactory={0.1} resetOnLeave className="w-full flex flex-col lg:flex-row justify-between items-center">
           <MouseParallaxChild factorX={0.1} factorY={0.1} >
-            <div className="flex flex-col text-center gap-6 lg:text-left">
+            <div className="flex flex-col text-center gap-8 lg:text-left ">
              
               <h1 className="text-3xl lg:text-6xl font-poppins font-extrabold text-indigo ">
               Boba Made With   <br/> Delight In Every Sip
@@ -24,7 +24,7 @@ const Banner = () => {
           <MouseParallaxChild factorX={0.1} factorY={0.1} >
           <div>
 
-            <div className="relative left-5">
+            <div className="relative left-5 top-6 lg:top-0">
               <Image src={'/Hero-Image.png'} width={560} height={560} alt='' priority={1}/>
             </div>
           </div>
--- client/src/app/components/CartDesktop.js
+++ client/src/app/components/CartDesktop.js
@@ -13,8 +13,8 @@ const CartDesktop = () => {
 
   return (
   
-  <div className={`${isOpen ? 'left-0 ' : '-left-full '} bg-white sm:hidden md:hidden fixed top-0 
-  bottom-0 w-[500px] shadow-2xl lg:flex flex-col transition-all duration-300 z-10 `}>
+  <div className={`${isOpen ? 'left-0 ' : '-left-full '} bg-white hidden fixed top-0 
+  bottom-0 w-[500px]  shadow-2xl lg:flex lg:visible flex-col transition-all duration-300 z-10 `}>
     
     <CartTop/>
 
