d34e133bf3f42b0b15cc94e3dd615839f8c77827
Millad Ayoubi <Milladayoubi@gmail.com>
2023-07-02 23:37:17 -0400

    Heading Update

--- client/src/app/components/SummerDrinks.js
+++ client/src/app/components/SummerDrinks.js
@@ -9,8 +9,8 @@ const SummerDrinks = () => {
   <section  className='section lg:mt-[-50px] ' id="crypto" data-aos="fade-right" data-aos-delay="500"data-aos-duration="1200">
     <div className='container mx-auto text-center lg:text-left' >
     <div className='flex flex-col items-center gap-x-10 lg:flex-row' >
-     <div className='lg:max-w-[700px] '>
-            <h2 className='section-title '>Summer Drink Days Are Back!</h2>
+     <div className='lg:max-w-[700px]'>
+            <h2 className='section-title'>Summer Drink Days Are Back!</h2>
             <p className='section-subtitle'>Sip away the summer heat and fill yourself with a nutritious drink with a small a smoothie only starting at $6.99! 
             </p>
             <button className='flex m-auto lg:m-0 gradient text-white rounded-full btn-sm font-semibold text-lg px-16 py-3'>Learn More</button>
