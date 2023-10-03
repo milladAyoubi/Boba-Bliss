4467bb1cfdaa7868f14b90ac95a9cbdb650e41c1
Millad Ayoubi <Milladayoubi@gmail.com>
2023-10-03 04:12:21 -0400

    Nav Update

--- client/src/app/components/Nav.js
+++ client/src/app/components/Nav.js
@@ -37,18 +37,17 @@ const Nav = () => {
        
             <div className="text-indigo font-medium text-medium">
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-transparent">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:gap-x-8 md:space-x-4 md:mt-0 md:mr-12 md:border-0 bg-transparent">
        
         <li>
-
-          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-white hover:duration-200 md:p-0 ">About</a>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 hover:text-secondary hover:duration-200 md:p-0 transition-all ">About</a>
         </li>
         <li>
-        <a href="#drinks" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-white hover:duration-200 md:p-0 ">Drinks</a>        </li>
+        <a href="#drinks" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-secondary hover:duration-200 md:p-0 transition-all">Drinks</a>        </li>
         <li>
-        <a href="#promos" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-white  hover:duration-200 md:p-0 ">Promos</a>        </li>
+        <a href="#promos" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-secondary  hover:duration-200 md:p-0 transition-all">Promos</a>        </li>
         <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-white hover:duration-200 md:p-0 ">Contact</a>        </li>
+        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-secondary hover:duration-200 md:p-0 transition-all">Contact</a>        </li>
       </ul>
     </div>
                 
